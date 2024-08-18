4b340c60f99c870ee81c36db96bc2a3d3b118157
Meher <82945138+Meher450@users.noreply.github.com>
2024-08-18 17:54:59 +0530

    Update Problem_3.cpp

--- Problem_3.cpp
+++ Problem_3.cpp
@@ -20,12 +20,10 @@ int main() {
     cout << "Wave print of the matrix is:\n";
     for (int j = 0; j < cols; j++) {
         if (j % 2 == 0) {
-            // Print top to bottom for even indexed columns
             for (int i = 0; i < rows; i++) {
                 cout << arr[i][j] << " ";
             }
         } else {
-            // Print bottom to top for odd indexed columns
             for (int i = rows - 1; i >= 0; i--) {
                 cout << arr[i][j] << " ";
             }
