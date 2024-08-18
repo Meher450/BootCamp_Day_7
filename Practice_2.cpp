91f888632725d7589a62135b7a3e436da281f6ed
Meher <82945138+Meher450@users.noreply.github.com>
2024-08-18 17:47:46 +0530

    Create Practice_2.cpp

---
+++ Practice_2.cpp
@@ -0,0 +1,29 @@
+//Sum of rows
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    int arr[rows][cols];
+    cout << "Enter the elements of the 2D array:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        int sum = 0;
+        for (int j = 0; j < cols; j++) {
+            sum += arr[i][j];
+        }
+        cout << "Sum of row " << i + 1 << " is: " << sum << endl;
+    }
+
+    return 0;
+}
